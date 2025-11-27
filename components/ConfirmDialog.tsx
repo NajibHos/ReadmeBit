6a468d60285352bc294fca335ff26c8021403ddb
najibhossn@gmail.com <najibhossn@gmail.com>
2025-11-27 19:01:42 +0600

    feat(dialog): add confirm dialog component

---
+++ components/ConfirmDialog.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useReadmeWidgets } from "@/lib/readme-context";
+import { Button } from "./ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+
+export default function ConfirmDialog() {
+
+  const { clearAll } = useReadmeWidgets();
+
+  return (
+    <div className="h-auto w-auto">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="secondary_red">Clear All</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you sure?</DialogTitle>
+            <DialogDescription>
+              All data will be lost. This action can not be undone.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="h-auto w-auto mt-1.5">
+            <Button onClick={() => clearAll()} variant="alert">
+              Yes, Clear all widgets
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
