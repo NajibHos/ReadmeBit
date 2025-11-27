161ed2b0ff22b30a64ae40bf4363acd00b94a725
najibhossn@gmail.com <najibhossn@gmail.com>
2025-11-27 18:58:00 +0600

    refactor(header): reduce logo font weight, update font family

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,16 +9,21 @@ export default function Header() {
       <div className="h-auto w-[90%] flex justify-between items-center">
         <div className="h-auto w-auto">
           <Link href='/'>
-            <h2 className="text-2xl lg:text-3xl font-geist! font-bold
-              text-gray-900! dark:text-white! cursor-pointer">
+            <h2 className="text-2xl lg:text-3xl font-workSans font-semibold
+              text-gray-900 dark:text-white cursor-pointer"
+            >
               ReadmeBit
-              <sub className="text-sm px-2 py-1 ml-2 bg-gray-100 dark:bg-gray-900 rounded">
+              <sub className="text-sm px-2 py-1 ml-2 bg-gray-100
+                dark:bg-gray-900 rounded"
+              >
                 Beta
               </sub>
             </h2>
           </Link>
         </div>
-        <div className="h-auto w-auto flex justify-center items-center gap-5">
+        <div className="h-auto w-auto flex justify-center items-center
+          gap-5"
+        >
           <div className="h-auto w-auto">
             <a
               href="#"
