5cc64224898463703c8607fcfc8b16702dd205a0
najibhossn@gmail.com <najibhossn@gmail.com>
2025-12-06 20:39:20 +0600

    Add sitemap.ts for SEO

---
+++ app/sitemap.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseURL = "readmebit.com";
+
+  return [
+    {
+      url: baseURL,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${baseURL}/create-readme`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseURL}/preview-readme`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseURL}/feedback`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+  ];
+}
