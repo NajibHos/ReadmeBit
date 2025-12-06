f9f0de424fdb225a2183db0a81b91cb9c16caf76
najibhossn@gmail.com <najibhossn@gmail.com>
2025-12-06 20:32:39 +0600

    style(cta button): update cta button scale animation and opacity

--- components/CTAButton.tsx
+++ components/CTAButton.tsx
@@ -1,27 +1,18 @@
 'use client';
 
 import Link from "next/link";
-import { motion } from "motion/react";
 
 export default function CTAButton() {
   return (
     <div className="h-auto w-auto mt-3 z-50!">
       <Link href="/create-readme">
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 1.0 }}
+        <button
           className="px-6 py-3 text-base font-workSans font-semibold
-          text-white bg-blue-700 border border-white/20 rounded-full"
+          text-white bg-blue-700 hover:opacity-90 border
+          border-white/20 rounded-full hover:scale-105"
         >
           Start Creating
-        </motion.button>
-        {/* <button
-          className="px-6 py-3 text-base font-workSans font-semibold
-          text-white bg-blue-700 border border-white/20 rounded-full
-          hover:scale-110 transition-all duration-300"
-        >
-          Start Creating
-        </button> */}
+        </button>
       </Link>
     </div>
   );
