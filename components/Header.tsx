3311e6cf1b302da96491b5aa6796a71925c5fe18
najibhossn@gmail.com <najibhossn@gmail.com>
2025-12-08 15:22:48 +0600

    feat(header): add github repo link in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,16 +9,6 @@ export default function Header() {
       <div className="h-auto w-[90%] flex justify-between items-center">
         <div className="h-auto w-auto">
           <Link href='/'>
-            {/* <h2 className="text-2xl lg:text-3xl font-workSans font-semibold
-              text-gray-900 dark:text-white cursor-pointer"
-            >
-              ReadmeBit
-              <sub className="text-sm px-2 py-1 ml-2 bg-gray-100
-                dark:bg-gray-900 rounded"
-              >
-                Beta
-              </sub>
-            </h2> */}
             <img
               src="/logo.png"
               alt="ReadmeBit Logo"
@@ -31,10 +21,10 @@ export default function Header() {
         >
           <div className="h-auto w-auto">
             <a
-              href="#"
+              href="https://github.com/NajibHos/ReadmeBit"
               target="_blank"
             >
-              <Button variant="secondary" size="icon" disabled>
+              <Button variant="secondary" size="icon">
                 <Github />
               </Button>
             </a>
