a36de1b0a9534de23731155ebb32005ae4f1bf2f
najibhossn@gmail.com <najibhossn@gmail.com>
2025-12-10 23:11:51 +0600

    update base url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseURL = "readmebit.com";
+  const baseURL = "https://readmebit.com";
 
   return [
     {
