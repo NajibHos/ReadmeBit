e8a706aa8e533918a796ae5a95874af886fdee78
najibhossn@gmail.com <najibhossn@gmail.com>
2025-12-28 19:12:59 +0600

    refactor(preview-readme): use copycode, download from custom hooks

--- app/preview-readme/page.tsx
+++ app/preview-readme/page.tsx
@@ -27,40 +27,7 @@ const jsonLd = {
 
 export default function PreviewReadme() {
   const { markdown } = useReadmeMarkdown();
-
-  const handleDownload = () => {
-    if (!markdown) {
-      toast.error('No markdown to download');
-      return;
-    } else {
-      toast.success('Downloading README.md');
-    }
-
-    // download markdown as README.md file
-    const blob = new Blob([markdown], { type: 'text/markdown' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'README.md';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-  };
-
-  const handleCopyCode = async () => {
-    try {
-      // copy to clipboard
-      await navigator.clipboard.writeText(markdown);
-      // toast notification
-      toast.success('Markdown copied to clipboard');
-    } catch (err) {
-      // toast notification
-      toast.error('Unable to copy markdown text');
-      // show error in console
-      console.error('Failed to copy markdown text: ', err);
-    }
-  };
+  const { handleDownload, handleCopyCode } = useReadmeActions(markdown);
 
   return (
     <div className="section-container">
