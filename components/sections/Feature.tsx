ada6a624118c204ea3d296dfb1858d0a7ab08454
najibhossn@gmail.com <najibhossn@gmail.com>
2026-07-11 14:45:58 +0600

    style(card spacing): reduce spacing between feature cards for better ux

--- components/sections/Feature.tsx
+++ components/sections/Feature.tsx
@@ -20,7 +20,7 @@ export default function Features() {
           </div>
         </div>
         <div className="h-auto w-full grid grid-cols-1 md:grid-cols-2
-          lg:grid-cols-3 gap-8"
+          lg:grid-cols-3 gap-4"
         >
           <Card className="h-auto w-full">
             <CardHeader>
